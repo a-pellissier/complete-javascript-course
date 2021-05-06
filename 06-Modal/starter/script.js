95e3df10597c75c886715c8c76173fa722e847f6
a-pellissier <achillepellissier@gmail.com>
2021-05-06 17:57:54 +0200

    progress

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,37 @@
 'use strict';
+
+//HTML objects
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsShowModal = document.querySelectorAll('.show-modal');
+
+//Hide and show modal window and overlay
+const closeModal = function () {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+};
+
+const openModal = function () {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+};
+
+//Add on-click action for show buttons
+for (let i = 0; i < btnsShowModal.length; i++) {
+    btnsShowModal[i].addEventListener('click', openModal);
+}
+
+//Add on-click action for close button and overlay
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+//Add escape key support
+document.addEventListener(
+    'keydown',
+    function (event) {
+        if (event.key == 'Escape' && !modal.classList.contains('hidden')) {
+            closeModal();
+        }
+    }
+);
